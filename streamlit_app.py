90586f4b72171dfe2c940ca6eec155ca53a20c9f
TimeJerker <krov1chh@gmail.com>
2026-03-21 12:39:26 +0400

    complete frontend (not tested)

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import inngest
 import os
 from pathlib import Path
 import time
+import requests
 
 st.set_page_config(page_title="RAG Ingest app",layout="centered")
 
@@ -36,8 +37,6 @@ async def send_path_to_rag_ingest_event(path: Path) -> None:
         )
     )
 
-
-
 st.title("Upload a PDF")
 uploaded = st.file_uploader("Choose a PDF", type=["pdf"], accept_multiple_files=False)
 
@@ -49,21 +48,57 @@ if uploaded:
 
     st.success("File uploaded!")
 
+async def send_rag_query_event(question: str):
+    client = get_Inngest_client()
+    result = await client.send(
+        inngest.Event(
+            name="rag/query_pdf_ai",
+            data={
+                "question": question,
+            },
+        )
+    )
+    return result[0]
+
+def _inngest_api_base() -> str:
+    return os.getenv("INNGEST_API_BASE", "http://127.0.0.1:8288/v1")
+
+def fetch_run(event_id: str) -> list[dict]:
+    url = f"{_inngest_api_base()}/events/{event_id}/runs"
+    answer = requests.get(url)
+    answer.raise_for_status()
+    data = answer.json()
+    return data.get("data", [])
+
+def wait_to_run_output(event_id: str, timeout_s: float = 120.0, poll_interval_s: float = 0.5) -> dict:
+    start = time.time()
+    last_status = None
+    while True:
+        runs = fetch_run(event_id)
+        if runs:
+            run = runs[0]
+            status = run.get("status")
+            last_status = status or last_status
+            if status in ("Completed", "Succeeded", "Success", "Finished"):
+                return run.get("output") or {}
+            if status in ("Failed", "Cancelled"):
+                raise RuntimeError(f"Function run {status}")
+            
+        if time.time() - start > timeout_s:
+            raise TimeoutError(f"Timed out eaiting for run output (last status {last_status})")
+        time.sleep(poll_interval_s)
+
 question = st.chat_input("Enter a question")
 
-if question:
-    with st.chat_message("user"):
-        st.write(question)
+with st.form(key="entering question"):
+    submitted = st.form_submit_button("Ask")
     
-    with st.chat_message("ai"):
-        st.write("Hello!")
+    if question:
+        with st.spinner("Sending event and generation answer..."):
+            event_id = asyncio.run(send_rag_query_event(question.strip))
+            output = wait_to_run_output(event_id)
+            answer = output.get("answer", "(No relevant responce)")
+            source = output.get("source", [])
 
-# with st.form(key="entering question"):
-#     submitted = st.form_submit_button("Ask")
-    
-#     if question:
-#         st.write("Nice")
-#         # with st.spinner("Sending event and generation answer..."):
-#         #     pass
-#         st.subheader("Answer")
-#         st.write()
+        st.subheader("Answer")
+        st.write(answer)
